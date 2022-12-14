4e8d49cbde423e65ff64e641e6bcc5c52e87aa58
RubTheKid <you@example.com>
2022-12-14 18:39:21 -0300

    update

--- Context/AppDbContext.cs
+++ Context/AppDbContext.cs
@@ -14,6 +14,29 @@ public class AppDbContext : DbContext
     //overriding ef default conventions
     protected override void OnModelCreating(ModelBuilder mb)
     {
+        //
+        mb.Entity<Categoria>().HasKey(c => c.CategoriaId);
 
+        mb.Entity<Categoria>().Property(c => c.Nome)
+                                .HasMaxLength(100)
+                                .IsRequired();
+        mb.Entity<Categoria>().Property(c => c.Descricao)
+                                .HasMaxLength(150)
+                                .IsRequired();
+        //
+        mb.Entity<Produto>().HasKey(p => p.ProdutoId);
+        mb.Entity<Produto>().Property(p => p.Nome)
+                                .HasMaxLength(150)
+                                .IsRequired();
+        mb.Entity<Produto>().Property(p => p.Descricao)
+                                .HasMaxLength(150);
+        mb.Entity<Produto>().Property(p => p.Imagem)
+                                .HasMaxLength(100);
+        mb.Entity<Produto>().Property(p => p.Preco)
+                                .HasPrecision(14, 2);
+        //
+        mb.Entity<Produto>().HasOne<Categoria>(c => c.Categoria)
+                            .WithMany(p => p.Produtos)
+                            .HasForeignKey(c => c.CategoriaId);
     }
 }
