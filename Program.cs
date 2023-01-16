f1436ee5d24f635cb89e27392ddbe44c7060d44f
RubTheKid <you@example.com>
2023-01-16 18:08:16 -0300

    update

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 var app = builder.Build();
 //define endpoints after build
 
-app.MapGet("/", () => "Catalogo de produtos");
+app.MapGet("/", () => "Catalogo de produtos").ExcludeFromDescription();
 
 app.MapPost("/categorias", async (Categoria categoria, AppDbContext db) =>
 {
@@ -28,6 +28,51 @@ app.MapPost("/categorias", async (Categoria categoria, AppDbContext db) =>
 
     return Results.Created($"/categorias/{categoria.CategoriaId}", categoria);
 });
+
+app.MapGet("/categorias", async (AppDbContext db) => await db.Categorias.ToListAsync());
+
+app.MapGet("/categorias/{id:int}", async (int id, AppDbContext db) =>
+{
+    return await db.Categorias.FindAsync(id)
+                 is Categoria categoria
+                 ? Results.Ok(categoria)
+                 : Results.NotFound();
+});
+
+app.MapPut("/categorias/{id:int}", async (int id,Categoria categoria, AppDbContext db) =>
+{
+    if (categoria.CategoriaId != id)
+    {
+        return Results.BadRequest();
+    }
+
+    var categoriaDB = await db.Categorias.FindAsync(id);
+
+    if (categoriaDB is null) return Results.NotFound();
+
+    categoriaDB.Nome = categoria.Nome;
+    categoriaDB.Descricao = categoria.Descricao;
+
+    await db.SaveChangesAsync();
+
+    return Results.Ok(categoriaDB);
+
+});
+
+app.MapDelete("/categorias/{id:int}", async (int id, AppDbContext db) =>
+{
+    var categoria = await db.Categorias.FindAsync(id);
+
+    if (categoria is null)  return Results.NotFound();
+
+    db.Categorias.Remove(categoria);
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+
+});
+
+
 
 
 // Configure the HTTP request pipeline.
@@ -38,4 +83,3 @@ if (app.Environment.IsDevelopment())
 }
 
 app.Run();
-
