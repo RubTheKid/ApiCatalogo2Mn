fc2adf309551c187b11ce0c4bf81378cf05bd24d
RubTheKid <you@example.com>
2023-01-16 15:30:47 -0300

    update

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,12 +1,14 @@
-﻿namespace ApiCatalogo2.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace ApiCatalogo2.Models;
 
 public class Categoria
 {
     public int CategoriaId { get; set; }
     public string? Nome { get; set; }
     public string? Descricao { get; set; }
-    
-    //settin 1:n relationship
+
+    [JsonIgnore]
     public ICollection<Produto>? Produtos { get; set; }
 
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using ApiCatalogo2.Context;
+using ApiCatalogo2.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +9,26 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options
+    .UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+
 var app = builder.Build();
+//define endpoints after build
+
+app.MapGet("/", () => "Catalogo de produtos");
+
+app.MapPost("/categorias", async (Categoria categoria, AppDbContext db) =>
+{
+    db.Categorias.Add(categoria);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/categorias/{categoria.CategoriaId}", categoria);
+});
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -14,6 +37,5 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
 app.Run();
 
